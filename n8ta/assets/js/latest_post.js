90bdbca52593317337cd2e306366fea23a650a34
n8ta <n8@u.northwestern.edu>
2020-12-27 00:25:52 -0600

    latest

---
+++ n8ta/assets/js/latest_post.js
@@ -0,0 +1,18 @@
+/*
+  Hide latest post if not recent
+
+*/
+
+
+console.info('run----')
+document.addEventListener('DOMContentLoaded',function() {
+    const latest_post = document.getElementById('latest-post')
+    if (latest_post) {
+        const now = Math.floor( new Date / 1000)
+        const posted_at = latest_post.dataset['time']
+        const diff = (now - posted_at) / (60*60*24)
+        if (diff < 7) {
+            latest_post.style.display = 'inline-block'
+        }
+    }
+})
