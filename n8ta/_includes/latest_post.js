e10b0a2c9e2262b0c5b57e71d81b188315a61655
Nathaniel Tracy-Amoroso <natetracyamoroso@gmail.com>
2022-04-11 23:57:40 -0700

    small html size improvements

--- n8ta/_includes/latest_post.js
+++ n8ta/_includes/latest_post.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded',function() {
-    const latest_post = document.getElementById('latest-post')
-    if (latest_post) {
+    const post = document.querySelector('.latest-posts')
+    if (post) {
         const now = Math.floor( new Date / 1000)
-        const posted_at = parseInt(latest_post.dataset['time'])
+        const posted_at = parseInt(post.dataset['time'])
         const diff = (now - posted_at) / (60*60*24)
         if (diff < 7) {
-            latest_post.style.display = 'inline-block'
+            post.style.display = 'inline-block'
         }
     }
 })
